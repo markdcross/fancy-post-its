ea4b9898910bb977cdc4ea7ab5190f227a8d3e28
MDC <markdcross@gmail.com>
2020-10-29 14:32:48 -0400

    GETs and POSTs functional, still need DELETE

--- Develop/server.js
+++ Develop/server.js
@@ -41,24 +41,32 @@ app.get('*', function (req, res) {
 });
 
 //* API POST Routes
-//TODO: POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
 app.post('/api/notes', function (req, res) {
+    // Capture user input
     let newNote = {
         title: req.body.title,
         text: req.body.text,
     };
+
+    // Convert current list of notes from JSON to an array, add the new note, convert back to JSON, and update db.json
     let noteList = JSON.parse(fs.readFileSync('db/db.json'));
     noteList.push(newNote);
     let newNoteList = JSON.stringify(noteList);
     fs.writeFileSync('db/db.json', newNoteList);
-    res.send(`Title: ${newNote.title}
-    Note: ${newNote.text}`);
+
+    // Send response
+    res.json(newNote);
+
+    // Send response
+    // res.send(`Title: ${newNote.title}
+    // Note: ${newNote.text}`);
 });
 
 //* API DELETE Routes
 //TODO: DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
 //TODO: This means you'll need to find a way to give each note a unique `id` when it's saved.
 //TODO: In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+// read all notes, parse to array, iterate through the array and assign the id as [i]?
 
 //* ===================================================
 //* Starts the server on PORT and begins listening
