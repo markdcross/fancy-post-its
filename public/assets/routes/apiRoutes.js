435d6dd5badea892f020dd115c35c08aa1435272
MDC <markdcross@gmail.com>
2020-11-02 09:53:08 -0500

    Updated API routes and added UUID for note ID in POST

---
+++ public/assets/routes/apiRoutes.js
@@ -0,0 +1,45 @@
+//* ===================================================
+//* Dependencies
+//* ===================================================
+const path = require('path');
+const uuid = require('uuid');
+
+//* ===================================================
+//* API Routes
+//* ===================================================
+module.exports = function (app) {
+    // GET all saved notes
+    app.get('/api/notes', function (req, res) {
+        fs.readFile('db/db.json', (err, data) => {
+            if (err) {
+                throw err;
+            } else {
+                res.json(JSON.parse(data));
+            }
+        });
+    });
+
+    // POST route for new note
+    app.post('/api/notes', function (req, res) {
+        // Capture user input
+        let newNote = {
+            title: req.body.title,
+            text: req.body.text,
+            id: uuid.v4(),
+        };
+
+        // Convert current list of notes from JSON to an array, add the new note, convert back to JSON, and update db.json
+        let noteList = JSON.parse(fs.readFileSync('db/db.json'));
+        noteList.push(newNote);
+        let newNoteList = JSON.stringify(noteList);
+        fs.writeFileSync('db/db.json', newNoteList);
+        // Send response
+        res.json(newNote);
+    });
+
+    // API DELETE Route
+    //TODO: DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
+    //TODO: This means you'll need to find a way to give each note a unique `id` when it's saved.
+    //TODO: In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
+    // read all notes, parse to array, iterate through the array and assign the id as [i]?
+};
--- server.js
+++ server.js
@@ -2,9 +2,10 @@
 //* Dependencies
 //* ===================================================
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
+//* ===================================================
+//* Express
+//* ===================================================
 // Sets up the Express App
 const app = express();
 const PORT = 4880;
@@ -15,45 +16,13 @@ app.use(express.json());
 
 // Middleware to ensure proper directory pathing for static elements
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/db'));
 
-//* ===================================================
-//* Routes
-//* ===================================================
-// Displays all saved notes
-app.get('/api/notes', function (req, res) {
-    fs.readFile('db/db.json', (err, data) => {
-        if (err) {
-            throw err;
-        } else {
-            res.json(JSON.parse(data));
-        }
-    });
-});
-
-
-
-// API POST Route
-app.post('/api/notes', function (req, res) {
-    // Capture user input
-    let newNote = {
-        title: req.body.title,
-        text: req.body.text,
-    };
-
-    // Convert current list of notes from JSON to an array, add the new note, convert back to JSON, and update db.json
-    let noteList = JSON.parse(fs.readFileSync('db/db.json'));
-    noteList.push(newNote);
-    let newNoteList = JSON.stringify(noteList);
-    fs.writeFileSync('db/db.json', newNoteList);
-    // Send response
-    res.json(newNote);
-});
-
-// API DELETE Route
-//TODO: DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete.
-//TODO: This means you'll need to find a way to give each note a unique `id` when it's saved.
-//TODO: In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-// read all notes, parse to array, iterate through the array and assign the id as [i]?
+//* ==============================================================================
+//* ROUTER
+//* ==============================================================================
+require('./public/assets/routes/apiRoutes')(app);
+require('./public/assets/routes/htmlRoutes')(app);
 
 //* ===================================================
 //* Starts the server on PORT and begins listening
